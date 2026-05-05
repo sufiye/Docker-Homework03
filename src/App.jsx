e7d818a4679e73f90ecf5a3f646748bb2e6ede01
sufiye <sufiye.huseynzade.20@gmail.com>
2026-05-05 18:28:39 +0400

    all

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-full w-fit border-rose-800 rounded-2xl border-2 ml-100 mt-50 p-5 '>
+      
+      <h1 className='text-3xl decoration-solid text-pink-500 text'>Sufiye Huseynzade Salamlar</h1>
+
+
+    </div>
+  )
+}
+
+export default App
